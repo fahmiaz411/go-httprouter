b31d9876bff592f2e926fa79f376dc07c00dd00a
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-24 13:43:25 +0700

    router.GET

---
+++ router_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRouter(t *testing.T) {
+	router := httprouter.New()
+
+	router.GET("/",func(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
+		fmt.Fprint(w, "Hello test")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Hello test", string(body))
+}
