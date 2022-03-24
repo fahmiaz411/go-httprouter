c0716aab719af07c7ef80a35195db97091aed5a8
fahmiaz411 <fahmiaziz884@gmail.com>
2022-03-24 13:43:42 +0700

    router.ServeFiles

---
+++ serve_file_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"embed"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed resources
+var resources embed.FS
+
+func TestServeFile(t *testing.T) {
+	router := httprouter.New()
+	dir, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(dir))
+
+	request := httptest.NewRequest("GET", "http://localhost:8080/files/good.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "good http router", string(body))
+}
